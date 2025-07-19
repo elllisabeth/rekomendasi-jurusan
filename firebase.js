76181109e018ff26a5d955dcb12db58bae74e98b
elllisabeth <fannydeboraid@gmail.com>
2025-07-19 16:22:28 +0700

    Initial commit: upload website rekomendasi jurusan

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Initialize Firebase (use your actual config from Firebase console)
+const firebaseConfig = {
+  apiKey: "AIzaSyCdybTE3x6yuWdLhDHcC5FU63zYRmwXMLI",
+  authDomain: "rekomendasijurusan-mutiara17.firebaseapp.com",
+  databaseURL: "https://rekomendasijurusan-mutiara17-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "rekomendasijurusan-mutiara17",
+  storageBucket: "rekomendasijurusan-mutiara17.firebasestorage.app",
+  messagingSenderId: "683109236007",
+  appId: "1:683109236007:web:2ff2ef98e9270e5f18bd02",
+  measurementId: "G-CL52DP4DSH"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
